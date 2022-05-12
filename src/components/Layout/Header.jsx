6550ca6954687abc043742a783b98da4c59b4afe
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-11 17:00:02 -0700

    very basic header, button not working

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,14 @@
 import { useItems } from '../../context/ShoppingProvider';
+import styles from './Header.css';
 
 export default function Header() {
   const { items } = useItems();
 
-  return <div>Items Needed to be Bought: {items.length}</div>;
+  return (
+    <section className={styles.header}>
+      <h2>Shopping List</h2>
+      <div>Items Needed to be Bought: {items.length}</div>
+      <button>clear list</button>
+    </section>
+  );
 }
