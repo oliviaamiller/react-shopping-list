12649a0fcb4a9cd55d91f5895840b5cc6d7275ce
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-12 11:32:27 -0700

    added onClick handleClearList to Header.jsx, clear list button working

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,13 +2,13 @@ import { useItems } from '../../context/ShoppingProvider';
 import styles from './Header.css';
 
 export default function Header() {
-  const { items } = useItems();
+  const { items, handleClearList } = useItems();
 
   return (
     <section className={styles.header}>
       <h2>Shopping List</h2>
       <div>Items Needed to be Bought: {items.length}</div>
-      <button>clear list</button>
+      <button onClick={() => handleClearList()}>clear list</button>
     </section>
   );
 }
