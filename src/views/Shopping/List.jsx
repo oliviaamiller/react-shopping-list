a05ceaf619954f79d6a4f0e600f71daf21ba78a0
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-11 17:16:25 -0700

    added form to add new item to List.jsx

--- src/views/Shopping/List.jsx
+++ src/views/Shopping/List.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useState } from 'react';
+import { useItems } from '../../context/ShoppingProvider';
+import Item from '../../components/Items/Item';
 
 export default function ShoppingList() {
+  const [newItem, setNewItem] = useState('');
+  const { items, handleAddItem, handleEditItem, handleDeleteItem } = useItems();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAddItem(newItem);
+    setNewItem('');
+  };
+
   return (
-    <div>ShoppingList</div>
-  )
+    <>
+      <h2>The List:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="newItem"
+          placeholder="Add an item"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button type="submit">Add Item</button>
+      </form>
+    </>
+  );
 }
