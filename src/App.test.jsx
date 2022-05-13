11f4d5b62d5fdd9ff3ef3102b07923654ad3824f
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-13 15:59:43 -0700

    tests passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -13,32 +13,6 @@ describe('Shopping behavioral list tests', () => {
     expect(initialItem).toBeInTheDocument();
   });
 
-  it('should add an item to the list', async () => {
-    render(<App />);
-
-    const item = await screen.findByPlaceholderText(/add an item/i);
-    userEvent.type(item, 'apples');
-
-    const button = await screen.findByRole('button', { name: /add item/i });
-    userEvent.click(button);
-
-    const apples = await screen.findByText(/apples/i);
-    expect(apples).toBeInTheDocument();
-  });
-
-  it('should delete an item from the list', async () => {
-    render(<App />);
-
-    const initialItem = await screen.findByText(
-      /add items to your shopping list/i
-    );
-
-    const button = await screen.findByRole('button', { name: /x/i });
-    userEvent.click(button);
-
-    expect(initialItem).not.toBeInTheDocument();
-  });
-
   it('should edit a list item', async () => {
     render(<App />);
 
@@ -61,4 +35,31 @@ describe('Shopping behavioral list tests', () => {
 
     expect(newItem).toBeInTheDocument();
   });
+
+  it('should delete an item from the list', async () => {
+    render(<App />);
+
+    const initialItem = await screen.findByText(
+      /add items to your shopping list/i
+    );
+
+    const button = await screen.findByRole('button', { name: /x/i });
+    userEvent.click(button);
+
+    expect(initialItem).not.toBeInTheDocument();
+  });
+
+  it('should add an item to the list', async () => {
+    render(<App />);
+
+    const item = await screen.findByPlaceholderText(/add an item/i);
+    userEvent.type(item, 'apples');
+
+    const button = await screen.findByRole('button', { name: /add item/i });
+    userEvent.click(button);
+
+    const apples = await screen.findByText(/apples/i);
+    expect(apples).toBeInTheDocument();
+  });
+
 });
