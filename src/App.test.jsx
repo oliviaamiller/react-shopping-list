d4abdebd605f99d96c23bf0cf82be6f6f4d098e3
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-13 15:20:27 -0700

    it should delete a list item test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,20 +3,29 @@ import App from './App';
 import userEvent from '@testing-library/user-event';
 
 describe('Shopping behavioral list tests', () => {
+  it('should add an item to the list', async () => {
+    render(<App />);
 
-    it('should add an item to the list', async () => {
-        render(<App />)
+    const item = await screen.findByPlaceholderText(/add an item/i);
+    userEvent.type(item, 'apples');
 
-        const item =  await screen.findByPlaceholderText(/add an item/i);
-        userEvent.type(item, 'apples');
+    const button = await screen.findByRole('button', { name: /add item/i });
+    userEvent.click(button);
 
-        const button = await screen.findByRole('button', { name: /add item/i});
-        userEvent.click(button);
+    const apples = await screen.findByText(/apples/i);
+    expect(apples).toBeInTheDocument();
+  });
 
-        const apples = await screen.findByText(/apples/i);
-        expect(apples).toBeInTheDocument();
+  it('should delete an item from the list', async () => {
+    render(<App />);
 
+    const initialItem = await screen.findByText(
+      /add items to your shopping list/i
+    );
 
-    })
+    const button = await screen.findByRole('button', { name: /x/i });
+    userEvent.click(button);
 
+    expect(initialItem).not.toBeInTheDocument();
+  });
 });
